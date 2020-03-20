463f7c8e353f81b9c578c0d534b5266d3e235bca
Jhon Arlex Ocampo Cifuentes <jhon.ocampo123@gmail.com>
2020-03-20 11:22:37 -0500

    opmizacion

--- android/capacitor_qrcode/build/generated/source/buildConfig/debug/com/jhon/capacitor_qrcode/qrcode/BuildConfig.java
+++ android/capacitor_qrcode/build/generated/source/buildConfig/debug/com/jhon/capacitor_qrcode/qrcode/BuildConfig.java
@@ -5,6 +5,11 @@ package com.jhon.capacitor_qrcode.qrcode;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.jhon.capacitor_qrcode.qrcode";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "com.jhon.capacitor_qrcode.qrcode";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
